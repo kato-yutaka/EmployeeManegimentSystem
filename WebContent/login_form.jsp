1f9316a8e15f266f16924355eeec38056b0722f5
aa <a>
2017-05-25 12:14:23 +0900

    hello

--- WebContent/login_form.jsp
+++ WebContent/login_form.jsp
@@ -3,12 +3,20 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset=UTF-8">
+		<meta charset="UTF-8">
 		<title>ログイン画面</title>
 	</head>
 	<body>
 		<h1>従業員管理システム</h1>
 		<hr>
-		<form >
+		<form action="UserManagementServlet" method="POST" >
+		<input type="hidden" name="ACTION" value="111">
+		ユーザー情報の入力<br>
+			<table>
+				<tr><td>UserName<input type="text" name="id"></td></tr>
+				<tr><td>PassWord<input type="text" name="password"></td></tr>
+				<tr><td><input type="submit" value="ログイン"></td></tr>
+			</table>
+		</form>
 	</body>
 </html>
--- src/servlet/UserManagementServlet.java
+++ src/servlet/UserManagementServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UserManagementServlet")
 public class UserManagementServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -35,7 +37,26 @@ public class UserManagementServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		// エンコーディング指定
+        request.setCharacterEncoding("Windows-31J");
+        response.setCharacterEncoding("Windows-31J");
+
+        // formからの値を取得
+        String action = request.getParameter("ACTION");
+        String id = request.getParameter("id");
+
+        // 移譲する先のjspを格納する変数url
+        String url = null;
+
+
+        if(action.equals("111")){
+        	url="menu.html";
+
+        }
+        //url="menu.html";
+        RequestDispatcher rd = request.getRequestDispatcher(url);
+        rd.forward(request, response);
 	}
 
 }
