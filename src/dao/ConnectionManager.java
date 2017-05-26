083882259a9aca6ce2c13de7b78d3c2a492a03a2
kato-yutaka <kato-yutaka@outlook.jp>
2017-05-26 12:06:08 +0900

    Test

--- src/dao/ConnectionManager.java
+++ src/dao/ConnectionManager.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionManager {
+public class ConnectionManager{
 
     private final static String URL = "jdbc:mysql://localhost/emp_sys_db";
     private final static String USER = "root";
